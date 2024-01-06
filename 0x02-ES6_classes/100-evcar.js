c3fa8fbab892a79664883600c9cfa1f14e6c9311
Sackitey68 <sackitey68@gmail.com>
2024-01-06 05:08:55 +0000

    fixing some errors

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,10 +3,20 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
